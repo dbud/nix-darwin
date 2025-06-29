8c1c23838330fc1dfe05ece591e1567b8bd13938
Dima Budaragin <d.budaragin+dev@gmail.com>
2025-06-29 09:46:23 +0100

    remove telegram overlay

--- flake.nix
+++ flake.nix
@@ -7,35 +7,15 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
     mac-app-util.url = "github:hraban/mac-app-util";
-
-    dtomvan-nixpkgs.url = "github:dtomvan/nixpkgs/dtomvan/push-zypzvrwlmlrm";
   };
 
-  outputs =
-    { self, nix-darwin, nix-homebrew, mac-app-util, dtomvan-nixpkgs, ... }:
-    let
-      system = "aarch64-darwin";
-
-      overlay-telegram-desktop = final: prev: {
-        telegram-desktop = (import dtomvan-nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        }).telegram-desktop;
-      };
-
-      pkgs = import nix-darwin.inputs.nixpkgs {
-        system = "aarch64-darwin";
-        overlays = [ overlay-telegram-desktop ];
-        config.allowUnfree = true;
-      };
-    in {
-      darwinConfigurations."dima-mba" = nix-darwin.lib.darwinSystem {
-        inherit pkgs;
-        modules = [
-          ({ pkgs, ... }: import ./configuration.nix { inherit self pkgs; })
-          nix-homebrew.darwinModules.nix-homebrew
-          mac-app-util.darwinModules.default
-        ];
-      };
+  outputs = { self, nix-darwin, nix-homebrew, mac-app-util, ... }: {
+    darwinConfigurations."dima-mba" = nix-darwin.lib.darwinSystem {
+      modules = [
+        ({ pkgs, ... }: import ./configuration.nix { inherit self pkgs; })
+        nix-homebrew.darwinModules.nix-homebrew
+        mac-app-util.darwinModules.default
+      ];
     };
+  };
 }
